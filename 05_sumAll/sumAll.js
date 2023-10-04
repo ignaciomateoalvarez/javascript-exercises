276e69fc9b16bf87d71c943ae7aec231eaa1c28f
Ignacio <ignacio.alvarez@snappler.com>
2023-10-04 16:14:40 -0300

    Complete exercises 01 to 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(initialNumber, finalNumber) {
+  let min = 0;
+  let max = 0;
+  let sum = 0;
 
+  if (!Number.isInteger(initialNumber) || !Number.isInteger(finalNumber)) return "ERROR";
+
+  if (initialNumber < 0 || finalNumber < 0) return 'ERROR'
+
+  if (initialNumber > finalNumber){
+    max = initialNumber;
+    min = finalNumber;
+  } else {
+    max = finalNumber;
+    min = initialNumber;
+  }
+
+  for (var i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  let validation = null;
+  if (year % 4 === 0) {
+    validation = true;
+    if (year % 100 === 0) {
+      validation = false;
+      if (year % 400 === 0) {
+        validation = true;
+      }
+    }
+  } else {
+    validation = false;
+  }
 
+  return validation
 };
 
 // Do not edit below this line
